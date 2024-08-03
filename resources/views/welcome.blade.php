008f63edc153d272b07442525ca8452b679e29a7
Jimmy <toluadejimi@gmail.com>
2024-08-03 17:39:22 +0100

    cc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
     <meta name="description" content="oprimeverify.com provides affordable, real US non-VoIP numbers for SMS verification on popular platforms like WhatsApp, eBay, Tinder, and more. Enjoy fast support, flexible pricing, and reliable service for all your SMS verification needs.">
 
-    <title>Bypass SMS and Text Verification With Real USA Numbers - OPRIME VERIFY</title>
+    <title>Bypass SMS and Text Verification With Real USA Numbers on - OPRIME VERIFY</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
