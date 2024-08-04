bcd0ff60ead9590ba0af9dc2d774b66a4ab4f063
Jimmy <toluadejimi@gmail.com>
2024-08-04 01:23:49 +0100

    vvv

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -306,9 +306,6 @@ function getOnlineSimServices() {
 function create_order($service, $price, $cost, $service_name, $cost2){
 // dd($service, $price, $cost, $service_name, $cost2,(int) auth()->user()->wallet);
 
-if ((int) Auth::user()->wallet==(int) $price) {
-    return 5;
-}
 
     if ((int) Auth::user()->wallet <(int) $price) {
         return 9;
@@ -345,10 +342,7 @@ if ((int) Auth::user()->wallet==(int) $price) {
         if(Auth::user()->wallet > $cost2){
             User::where('id', Auth::id())->decrement('wallet', $cost2);
             User::where('id', Auth::id())->increment('hold_wallet', $cost2);
-        }else{
-            return 9;
         }
-
 
         $ver = new Verification();
         $ver->user_id = Auth::id();
