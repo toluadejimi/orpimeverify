3ba4a6008e7a66ae620d281ad46474ea0568657b
Jimmy <toluadejimi@gmail.com>
2024-08-06 21:35:47 +0100

    kjhgf

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -125,7 +125,25 @@
                             {{ session()->get('error') }}
                         </div>
                         @endif
-
+                        <script src="{{ url('') }}/public/concept/assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+    <!-- bootstap bundle js -->
+    <script src="{{ url('') }}/public/concept/assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+    <!-- slimscroll js -->
+    <script src="{{ url('') }}/public/concept/assets/vendor/slimscroll/jquery.slimscroll.js"></script>
+    <!-- main js -->
+    <script src="{{ url('') }}/public/concept/assets/libs/js/main-js.js"></script>
+    <!-- chart chartist js -->
+    <script src="{{ url('') }}/public/concept/assets/vendor/charts/chartist-bundle/chartist.min.js"></script>
+    <!-- sparkline js -->
+    <script src="{{ url('') }}/public/concept/assets/vendor/charts/sparkline/jquery.sparkline.js"></script>
+    <!-- morris js -->
+    <script src="{{ url('') }}/public/concept/assets/vendor/charts/morris-bundle/raphael.min.js"></script>
+    <script src="{{ url('') }}/public/concept/assets/vendor/charts/morris-bundle/morris.js"></script>
+    <!-- chart c3 js -->
+    <script src="{{ url('') }}/public/concept/assets/vendor/charts/c3charts/c3.min.js"></script>
+    <script src="{{ url('') }}/public/concept/assets/vendor/charts/c3charts/d3-5.4.0.min.js"></script>
+    <script src="{{ url('') }}/public/concept/assets/vendor/charts/c3charts/C3chartjs.js"></script>
+    <script src="{{ url('') }}/public/concept/assets/libs/js/dashboard-ecommerce.js"></script>
 
     <div class="bg-base-100 border-t mt-5 p-5">
         <footer class="d-flex justify-content-center ">
