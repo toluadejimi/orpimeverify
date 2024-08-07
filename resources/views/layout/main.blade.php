035f918d859d3622b4667bb43bf3a700b9adb172
Jimmy <toluadejimi@gmail.com>
2024-08-07 11:04:26 +0100

    sdfgh

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -432,7 +432,38 @@
     </script>
 
 
-   
+    <style>
+        .float {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 40px;
+            background-color: #000000;
+            color: #FFF;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
+
+        .my-float {
+            margin-top: 16px;
+        }
+    </style>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <a href="https://t.me/+fTn69pDmZuc1MjM0" class="float" target="_blank">
+        <i class="fa fa-comment my-float"></i>
+    </a>
+
+
+    <script>
+        .modal - backdrop {
+            z - index: -1;
+        }
+    </script>
 
 
     <div class="bg-base-100 border-t mt-5 p-5">
