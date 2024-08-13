cfc8622d76a219a48ecf837d6359b2dff79db4bc
Jimmy <toluadejimi@gmail.com>
2024-08-13 21:09:20 +0100

    kjhgf

--- resources/views/fund-wallet.blade.php
+++ resources/views/fund-wallet.blade.php
@@ -59,7 +59,7 @@
                                 <label class="my-2 mt-4">Select Payment mode</label>
                                 <select name="type" class="form-control">
                                     <!--<option value="1">Enkpay</option>-->
-                                    <option value="3">FlutterWave</option>
+                                    <!-- <option value="3">FlutterWave</option> -->
                                     <!--<option value="4">GetPay</option>-->
                                     <option value="1">Enkpay</option>
                                     <option value="2">Manual</option>
