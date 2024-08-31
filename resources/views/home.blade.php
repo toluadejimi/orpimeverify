80e2aafb27d46ab715e890687754bc4e866b62ac
Jimmy <toluadejimi@gmail.com>
2024-08-31 17:54:01 +0100

    gh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,11 @@
 
 <br>
 @if(!auth()->user()->is_verified)
+@if (session()->has('message'))
+    <div class="alert alert-success">
+        {{ session()->get('message') }}
+    </div>
+@endif
 <br>
 <br>
 <div>
