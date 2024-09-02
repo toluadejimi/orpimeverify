16912be649e242aa608c09dbcab04548632c71f5
Jimmy <toluadejimi@gmail.com>
2024-09-02 20:25:23 +0100

    dfg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -380,11 +380,7 @@
 
                         <hr style="border-color: #cccccc" class=" my-2">
                     </div>
-                    <!-- @endforeach
-
-                    @else
-                    <span>Server busy</span>
-                    @endif -->
+                
                 </div>
                 <div x-show="currentTab === 3" style="height:200px; width:100%; overflow-y: scroll;" class="">
 
