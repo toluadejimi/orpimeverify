f0f3eebe1e5873553562b85d1ef5d5a7f9481832
Jimmy <toluadejimi@gmail.com>
2024-09-03 09:22:49 +0100

    xcvb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -458,30 +458,8 @@
 
                         </div>
                         <div class="pt-2" id="servicesList">
-                        <button class="myButton"
-                                        style="border: 0px; background: transparent"
-                                        onclick="hideButtondis(this)"><i
-                                            class="fa fa-shopping-bag"></i>Buy </button>
-                                            @endif
+                            
 
-@else
-
-<a class=""
-    href="/login">
-    <i class="fa fa-lock text-dark"></i>
-</a>
-@endauth
-
-<script>
-    function hideButtondis(link) {
-        // Hide the clicked link
-        link.style.display = 'none';
-
-        setTimeout(function() {
-            link.style.display = 'inline'; // or 'block' depending on your layout
-        }, 5000); // 5 seconds
-    }
-</script>
                         </div>
                     </div>
 
